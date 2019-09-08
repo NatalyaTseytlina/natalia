b5c9425376ed52d89007c02c263895b7f0e23156
natalia <natashaceitlina@mail.ru>
2019-09-08 04:21:53 +0300

    feat(web): env for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  instagramApiPath: 'https://api.instagram.com',
+  instagramPath: 'https://www.instagram.com/',
+  instagramAccessToken: '1735247404.60e0739.c5d7b2837bcc478a8ce03af6235eb2aa',
+  instagramAccountName: 'natali_tseytlina'
 };
